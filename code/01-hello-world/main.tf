31e74155073d9b625b2da44368058898504ec3ef
Stefan Mikula <stef.mikula@gmail.com>
2024-07-22 21:23:20 +0200

    single instance ok

--- code/01-hello-world/main.tf
+++ code/01-hello-world/main.tf
@@ -1,10 +1,10 @@
 # Configure the AWS provider
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-central-1"
 }
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-785db401"
+  ami           = "ami-0346fd83e3383dcb4"
   instance_type = "t2.micro"
 }
